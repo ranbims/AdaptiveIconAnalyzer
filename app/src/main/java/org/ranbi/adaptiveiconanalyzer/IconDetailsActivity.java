22329d7a0af35cfcfb3026b24422706a9d7136fa
Ran Bi <ranbi@microsoft.com>
2020-08-07 19:02:50 +0800

    Extract colors from the drawable

--- app/src/main/java/org/ranbi/adaptiveiconanalyzer/IconDetailsActivity.java
+++ app/src/main/java/org/ranbi/adaptiveiconanalyzer/IconDetailsActivity.java
@@ -1,19 +1,31 @@
 package org.ranbi.adaptiveiconanalyzer;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.graphics.drawable.AdaptiveIconDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.palette.graphics.Palette;
 
 public class IconDetailsActivity extends AppCompatActivity {
+
+    private static final int BITMAP_SIZE = 48;
 
     private ImageView mRawIcon;
     private ImageView mForeground;
@@ -55,8 +67,66 @@ public class IconDetailsActivity extends AppCompatActivity {
                 mForegroundDisplayArea.setVisibility(View.GONE);
                 mBackgroundDisplayArea.setVisibility(View.GONE);
                 mNonAdaptiveWarning.setVisibility(View.VISIBLE);
+
+                // regard both foreground and background as the raw icon
+                mForegroundDrawable = mIconDrawable;
+                mBackgroundDrawable = mIconDrawable;
             }
             mRawIcon.setImageDrawable(mIconDrawable);
         }
+
+//        int bitmapSize = dip2px(this, BITMAP_SIZE);
+        int bitmapSize = 120;
+
+        Bitmap paletteBackgroundBitmap = Bitmap.createBitmap(bitmapSize, bitmapSize, Bitmap.Config.ARGB_8888);
+        Canvas backgroundCanvas = new Canvas(paletteBackgroundBitmap);
+        Rect bgBounds = mBackgroundDrawable.getBounds();
+        mBackgroundDrawable.setBounds(0, 0, bitmapSize, bitmapSize);
+        mBackgroundDrawable.draw(backgroundCanvas);
+        mBackgroundDrawable.setBounds(bgBounds);
+
+        Bitmap paletteForegroundBitmap = Bitmap.createBitmap(bitmapSize, bitmapSize, Bitmap.Config.ARGB_8888);
+        Canvas foregroundCanvas = new Canvas(paletteForegroundBitmap);
+        Rect fgBounds = mForegroundDrawable.getBounds();
+        mForegroundDrawable.setBounds(0, 0, bitmapSize, bitmapSize);
+        mForegroundDrawable.draw(foregroundCanvas);
+        mForegroundDrawable.setBounds(fgBounds);
+
+        Bitmap paletteRawBitmap = Bitmap.createBitmap(bitmapSize, bitmapSize, Bitmap.Config.ARGB_8888);
+        Canvas rawCanvas = new Canvas(paletteRawBitmap);
+        Rect rawBounds = mIconDrawable.getBounds();
+        mIconDrawable.setBounds(0, 0, bitmapSize, bitmapSize);
+        mIconDrawable.draw(rawCanvas);
+        mIconDrawable.setBounds(rawBounds);
+
+        initializeColorContents((LinearLayout) findViewById(R.id.icon_background_color_list), paletteBackgroundBitmap);
+        initializeColorContents((LinearLayout) findViewById(R.id.icon_foreground_color_list), paletteForegroundBitmap);
+        initializeColorContents((LinearLayout) findViewById(R.id.icon_raw_icon_color_list), paletteRawBitmap);
+    }
+
+    private void initializeColorContents(LinearLayout linearLayout, Bitmap bitmap) {
+        Palette.Builder builder = Palette.from(bitmap);
+        Palette palette = builder.generate();
+        linearLayout.addView(initializeColorInfo("Dominent:", palette.getDominantColor(Color.WHITE)));
+        linearLayout.addView(initializeColorInfo("Light muted:", palette.getLightMutedColor(Color.WHITE)));
+        linearLayout.addView(initializeColorInfo("Light vibrant:", palette.getLightVibrantColor(Color.WHITE)));
+    }
+
+    private View initializeColorInfo(String title, int color) {
+        View view = LayoutInflater.from(this).inflate(R.layout.color_info, null);
+        ((TextView) view.findViewById(R.id.color_title)).setText(title);
+        ((TextView) view.findViewById(R.id.color_value)).setText("#" + Integer.toHexString(color));
+        ImageView imageView = view.findViewById(R.id.color_view);
+        imageView.setImageDrawable(new ColorDrawable(color));
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                0, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutParams.weight = 1;
+        view.setLayoutParams(layoutParams);
+        return view;
+    }
+
+    public static int dip2px(Context context, float dpValue) {
+        return (int) (dpValue * context.getResources().getDisplayMetrics().density + 0.5f);
     }
 }
---
+++ app/src/main/java/org/ranbi/adaptiveiconanalyzer/SquareImageView.java
@@ -0,0 +1,34 @@
+package org.ranbi.adaptiveiconanalyzer;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+
+@SuppressLint("AppCompatCustomView")
+public class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SquareImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        setMeasuredDimension(width, width);
+    }
+
+}
